2b8c803767bc0a984b2cce393b03079fc8aa9263
koya1616 <koya1616.0622@gmail.com>
2024-08-25 17:05:36 +0700

    .hirate_storeフォルダーに機密情報を保存するようにする

--- src/models/local_storage.rs
+++ src/models/local_storage.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{Read, Write};
 
+use crate::runtime_config::RuntimeConfig;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Auth {
   pub access_token: String,
@@ -24,16 +26,16 @@ impl Auth {
 
   pub fn save_data(&self) -> std::io::Result<()> {
     let json = serde_json::to_string(self)?;
-    let mut file = File::create("user.json")?;
+    let mut file = File::create(RuntimeConfig::global().config_folder.join("auth.json"))?;
     file.write_all(json.as_bytes())?;
     Ok(())
   }
 
   pub fn read_data(&self) -> std::io::Result<Auth> {
-    let mut file = File::open("user.json")?;
+    let mut file = File::open(RuntimeConfig::global().config_folder.join("auth.json"))?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    let user: Auth = serde_json::from_str(&contents)?;
+    let user: Self = serde_json::from_str(&contents)?;
     Ok(user)
   }
 }
--- src/runtime_config.rs
+++ src/runtime_config.rs
@@ -1,12 +1,22 @@
+use std::env;
+use std::path::PathBuf;
 use std::sync::OnceLock;
 
 pub struct RuntimeConfig {
   pub supabase_url: String,
   pub supabase_key: String,
   pub hirata_store_api_url: String,
+  pub config_folder: PathBuf,
 }
 
 impl RuntimeConfig {
+  fn get_config_file_path() -> PathBuf {
+    env::var("HOME")
+      .map(PathBuf::from)
+      .unwrap_or_else(|_| PathBuf::from("."))
+      .join(".hirata_store")
+  }
+
   pub fn global() -> &'static RuntimeConfig {
     static INSTANCE: OnceLock<RuntimeConfig> = OnceLock::new();
     INSTANCE.get_or_init(|| {
@@ -14,6 +24,7 @@ impl RuntimeConfig {
         supabase_url: "https://awwcgrfqoklqbgzzkawx.supabase.co".to_string(),
         supabase_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3d2NncmZxb2tscWJnenprYXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzU0ODMsImV4cCI6MjAzOTY1MTQ4M30.AP31pL-Ed94gkM32ttFFiTUwVwZri5KDkXoWgFV022U".to_string(),
         hirata_store_api_url: "https://minimal-tish-hirata-store-eaeab429.koyeb.app".to_string(),
+        config_folder: Self::get_config_file_path(),
       }
     })
   }
