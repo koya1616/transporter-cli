7132d0c905025ccdd3407f4ae47c4a80a724e052
koya1616 <koya1616.0622@gmail.com>
2024-08-25 15:08:46 +0700

    平田ストアAPIの定義

--- src/runtime_config.rs
+++ src/runtime_config.rs
@@ -3,6 +3,7 @@ use std::sync::OnceLock;
 pub struct RuntimeConfig {
   pub supabase_url: String,
   pub supabase_key: String,
+  pub hirata_store_api_url: String,
 }
 
 impl RuntimeConfig {
@@ -12,6 +13,7 @@ impl RuntimeConfig {
       RuntimeConfig {
         supabase_url: "https://awwcgrfqoklqbgzzkawx.supabase.co".to_string(),
         supabase_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3d2NncmZxb2tscWJnenprYXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQwNzU0ODMsImV4cCI6MjAzOTY1MTQ4M30.AP31pL-Ed94gkM32ttFFiTUwVwZri5KDkXoWgFV022U".to_string(),
+        hirata_store_api_url: "https://minimal-tish-hirata-store-eaeab429.koyeb.app".to_string(),
       }
     })
   }
